45818b0f3f1372e8b666ebc5a0e346f28347b231
Roberto <robertoanfuso98@gmail.com>
2021-05-26 16:45:51 +0200

    stampa su html

--- js/myscript.js
+++ js/myscript.js
@@ -4,5 +4,9 @@ var cognome = prompt("inserisci cognome");
 
 var colore = prompt("inserisci colore preferito");
 
-alert(nome+cognome+colore+21);
+var password=nome+cognome+colore+21;
+
+document.getElementById("testo").innerHTML=password;
+
+
 
